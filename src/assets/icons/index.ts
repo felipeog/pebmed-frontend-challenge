5818f9a7141f5e2d67c526357d3536970723c857
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-15 17:27:59 -0300

    chore(icons): update icons to be used in the icon component

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as ArrowLeft } from "./arrowLeft.svg";
+export { ReactComponent as Check } from "./check.svg";
+export { ReactComponent as Iugu } from "./Iugu.svg";
+export { ReactComponent as QuestionMark } from "./questionMark.svg";
+export { ReactComponent as Star } from "./star.svg";
+export { ReactComponent as Whitebook } from "./whitebook.svg";
