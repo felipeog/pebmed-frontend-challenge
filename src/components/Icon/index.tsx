95a348d6d26c7b6f900e97d94950179923f68359
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-15 20:59:07 -0300

    fix(icon): add default fill

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -6,14 +6,14 @@ interface IIconProps extends ComponentProps<"svg"> {
   name: "ArrowLeft" | "Check" | "Iugu" | "QuestionMark" | "Star" | "Whitebook";
 }
 
-function Icon({ name, ...props }: IIconProps) {
+function Icon({ name, fill = "var(--color_primary)", ...props }: IIconProps) {
   const CurrentIcon = Icons?.[name];
 
   if (!CurrentIcon) {
     return null;
   }
 
-  return <CurrentIcon className={`Icon ${name}`} {...props} />;
+  return <CurrentIcon className={`Icon ${name}`} fill={fill} {...props} />;
 }
 
 export { Icon };
