27b248ec78efcc4828575ececd67acc2e71cd972
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-15 15:33:03 -0300

    chore(layout): add layout tokens

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import "./main.css";
 import { App } from "./App";
 
 const root = document.getElementById("root") as HTMLElement;
