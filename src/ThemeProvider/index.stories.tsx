34038290edc4144299baaf60bae212b5696b140b
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-15 19:37:27 -0300

    refactor(theme provider): update stories

--- src/ThemeProvider/index.stories.tsx
+++ src/ThemeProvider/index.stories.tsx
@@ -15,34 +15,44 @@ type Story = StoryObj<typeof meta>;
 export const Color: Story = {
   args: {
     children: (
-      <div style={{ display: "grid", gap: "1rem" }}>
-        <p style={{ color: "var(--color_primary)" }}>
-          --color_primary - &#11044;
-        </p>
-        <p style={{ color: "var(--color_secondary)" }}>
-          --color_secondary - &#11044;
-        </p>
+      <div>
+        <code>--color_primary</code>
+        <p style={{ color: "var(--color_primary)" }}>&#11044;</p>
+        <br />
+
+        <code>--color_secondary</code>
+        <p style={{ color: "var(--color_secondary)" }}>&#11044;</p>
+        <br />
+
+        <code>--color_white</code>
         <p
           style={{
             color: "var(--color_white)",
             backgroundColor: "var(--color_gray_2)",
           }}
         >
-          --color_white - &#11044;
+          &#11044;
         </p>
-        <p style={{ color: "var(--color_gray_1)" }}>
-          --color_gray_1 - &#11044;
-        </p>
-        <p style={{ color: "var(--color_gray_2)" }}>
-          --color_gray_2 - &#11044;
-        </p>
-        <p style={{ color: "var(--color_gray_3)" }}>
-          --color_gray_3 - &#11044;
-        </p>
-        <p style={{ color: "var(--color_gray_4)" }}>
-          --color_gray_4 - &#11044;
-        </p>
-        <p style={{ color: "var(--color_black)" }}>--color_black - &#11044;</p>
+        <br />
+
+        <code>--color_gray_1</code>
+        <p style={{ color: "var(--color_gray_1)" }}>&#11044;</p>
+        <br />
+
+        <code>--color_gray_2</code>
+        <p style={{ color: "var(--color_gray_2)" }}>&#11044;</p>
+        <br />
+
+        <code>--color_gray_3</code>
+        <p style={{ color: "var(--color_gray_3)" }}>&#11044;</p>
+        <br />
+
+        <code>--color_gray_4</code>
+        <p style={{ color: "var(--color_gray_4)" }}>&#11044;</p>
+        <br />
+
+        <code>--color_black</code>
+        <p style={{ color: "var(--color_black)" }}>&#11044;</p>
       </div>
     ),
   },
@@ -51,39 +61,56 @@ export const Color: Story = {
 export const Typography: Story = {
   args: {
     children: (
-      <div style={{ display: "grid", gap: "1rem" }}>
-        <p style={{ font: "var(--heading_1)" }}>
-          --heading_1 - Lorem ipsum dolor sit amet
-        </p>
-        <p style={{ font: "var(--heading_2)" }}>
-          --heading_2 - Lorem ipsum dolor sit amet
-        </p>
-        <p style={{ font: "var(--heading_3)" }}>
-          --heading_3 - Lorem ipsum dolor sit amet
-        </p>
-        <p style={{ font: "var(--heading_4)" }}>
-          --heading_4 - Lorem ipsum dolor sit amet
-        </p>
-        <p style={{ font: "var(--heading_5)" }}>
-          --heading_5 - Lorem ipsum dolor sit amet
-        </p>
-        <p style={{ font: "var(--heading_6)" }}>
-          --heading_6 - Lorem ipsum dolor sit amet
-        </p>
+      <div>
+        <code>--heading_1</code>
+        <p style={{ font: "var(--heading_1)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--heading_2</code>
+        <p style={{ font: "var(--heading_2)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--heading_3</code>
+        <p style={{ font: "var(--heading_3)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--heading_4</code>
+        <p style={{ font: "var(--heading_4)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--heading_5</code>
+        <p style={{ font: "var(--heading_5)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--heading_6</code>
+        <p style={{ font: "var(--heading_6)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--text_regular</code>
         <p style={{ font: "var(--text_regular)" }}>
-          --text_regular - Lorem ipsum dolor sit amet
+          Lorem ipsum dolor sit amet
         </p>
-        <p style={{ font: "var(--text_small)" }}>
-          --text_small - Lorem ipsum dolor sit amet
-        </p>
+        <br />
+
+        <code>--text_small</code>
+        <p style={{ font: "var(--text_small)" }}>Lorem ipsum dolor sit amet</p>
+        <br />
+
+        <code>--text_small_bold</code>
         <p style={{ font: "var(--text_small_bold)" }}>
-          --text_small_bold - Lorem ipsum dolor sit amet
+          Lorem ipsum dolor sit amet
         </p>
+        <br />
+
+        <code>--text_footnote</code>
         <p style={{ font: "var(--text_footnote)" }}>
-          --text_footnote - Lorem ipsum dolor sit amet
+          Lorem ipsum dolor sit amet
         </p>
+        <br />
+
+        <code>--text_exception</code>
         <p style={{ font: "var(--text_exception)" }}>
-          --text_exception - Lorem ipsum dolor sit amet
+          Lorem ipsum dolor sit amet
         </p>
       </div>
     ),
