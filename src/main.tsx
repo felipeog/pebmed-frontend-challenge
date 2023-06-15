4b6b28c81e8d5b6bf20989943e56b72741e7c15f
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-15 16:24:35 -0300

    chore(theme provider): update styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
 import { ThemeProvider } from "./ThemeProvider";
-import "./main.css";
 
 const root = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(root).render(
