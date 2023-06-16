82b77fd2fdfe67480710e83aa61e781246370d7c
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-16 20:49:21 -0300

    chore(select): create select component

---
+++ src/components/Select/index.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Select } from "components/Select";
+
+const meta = {
+  title: "Component/Select",
+  component: Select,
+  tags: ["component"],
+  argTypes: {},
+} satisfies Meta<typeof Select>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Selects: Story = {
+  args: {
+    disabled: false,
+    label: "Label",
+    id: "ID",
+    placeholder: "Selecionar",
+    required: true,
+    isDefaultValue: false,
+    options: [
+      {
+        label: "Label 1",
+        value: "Value 1",
+      },
+      {
+        label: "Label 2",
+        value: "Value 2",
+      },
+    ],
+  },
+};
---
+++ src/components/Select/index.tsx
@@ -0,0 +1,65 @@
+import { ComponentPropsWithRef, forwardRef, Ref } from "react";
+import clsx from "clsx";
+
+import styles from "./index.module.css";
+
+interface IOption {
+  value: string | number;
+  label: string | number;
+}
+interface ISelectProps extends ComponentPropsWithRef<"select"> {
+  label?: string;
+  options: IOption[];
+  isValid?: boolean;
+  isDefaultValue?: boolean;
+}
+
+function Select(
+  {
+    className,
+    disabled = false,
+    label,
+    id,
+    options,
+    isValid = true,
+    isDefaultValue = false,
+    ...props
+  }: ISelectProps,
+  ref: Ref<HTMLSelectElement>
+) {
+  return (
+    <div className={clsx("Select", className, styles.wrapper)}>
+      {label && (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+
+      <select
+        ref={ref}
+        className={clsx(styles.select, {
+          [styles.error]: !isValid,
+          [styles.placeholder]: isDefaultValue,
+        })}
+        id={id}
+        disabled={disabled}
+        defaultValue={""}
+        {...props}
+      >
+        <option value="" disabled hidden>
+          Selecionar
+        </option>
+
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+const SelectWithRef = forwardRef(Select);
+
+export { SelectWithRef as Select };
