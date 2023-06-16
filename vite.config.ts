45f5d712218c4d29bed043cc5fcabf132610d9da
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-16 00:52:59 -0300

    chore(vite): configure alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "./src/assets/"),
+      components: path.resolve(__dirname, "./src/components/"),
+      pages: path.resolve(__dirname, "./src/pages/"),
+    },
+  },
 });
