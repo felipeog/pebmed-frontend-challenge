7a80f44361b849dc68f2904da4106638786cc803
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-17 19:17:06 -0300

    chore(cypress): update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,8 +8,6 @@ export default defineConfig({
     },
   },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:5173",
   },
 });
