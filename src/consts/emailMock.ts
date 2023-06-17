b656893e467715077584c2ddcf7934b2be89d4cc
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-17 14:02:28 -0300

    chore(conts): create consts

---
+++ src/consts/emailMock.ts
@@ -0,0 +1,1 @@
+export const EMAIL_MOCK = "usuario@email.com.br";
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       assets: path.resolve(__dirname, "./src/assets/"),
       components: path.resolve(__dirname, "./src/components/"),
+      consts: path.resolve(__dirname, "./src/consts/"),
       pages: path.resolve(__dirname, "./src/pages/"),
       router: path.resolve(__dirname, "./src/router/"),
       services: path.resolve(__dirname, "./src/services/"),
