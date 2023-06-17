c6dbb78e66696322ba642cc11c1941542b265f3a
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-16 22:50:16 -0300

    chore(package): add react tooltip

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { App } from "./App";
 import { ThemeProvider } from "theme/ThemeProvider";
+import "react-tooltip/dist/react-tooltip.css";
 
 const queryClient = new QueryClient();
 const root = document.getElementById("root") as HTMLElement;
