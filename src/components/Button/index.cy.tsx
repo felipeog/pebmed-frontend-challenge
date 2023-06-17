1ecb1b083dce5bf80565661b08099f90dce3702f
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-17 17:27:06 -0300

    chore(button): finish unit test

--- src/components/Button/index.cy.tsx
+++ src/components/Button/index.cy.tsx
@@ -4,6 +4,29 @@ describe("<Button />", () => {
   it("should render correctly", () => {
     cy.mount(<Button>Button</Button>);
 
-    cy.get(".Button").should("be.visible");
+    cy.get(".Button").should("be.visible").should("have.text", "Button");
+  });
+
+  it("should trigger onClick", () => {
+    const onClick = cy.stub().as("onClick");
+
+    cy.mount(<Button onClick={onClick}>Button</Button>);
+
+    cy.get(".Button").click();
+    cy.get("@onClick").should("be.called");
+  });
+
+  it("should not trigger onClick when disabled", () => {
+    const onClick = cy.stub().as("onClick");
+
+    cy.mount(
+      <Button onClick={onClick} disabled>
+        Button
+      </Button>
+    );
+
+    cy.get(".Button").should("be.disabled");
+    cy.get(".Button").click({ force: true });
+    cy.get("@onClick").should("not.be.called");
   });
 });
