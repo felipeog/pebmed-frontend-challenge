623397a7962fa039149bfb92f7bad833a318272b
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-18 15:33:00 -0300

    chore(cypress): update test

--- cypress/e2e/pages/checkout.cy.ts
+++ cypress/e2e/pages/checkout.cy.ts
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 
-describe("checkout", () => {
-  const baseUrl = "https://private-0ced4-pebmeddesafiofrontend.apiary-mock.com";
+const BASE_URL = "https://private-0ced4-pebmeddesafiofrontend.apiary-mock.com";
+
+describe("/checkout - success", () => {
   const mockPlan = {
     id: 0,
     storeId: "id",
@@ -23,7 +24,7 @@ describe("checkout", () => {
   };
 
   it("should render splittable form", () => {
-    cy.intercept(`${baseUrl}/offer`, (req) => {
+    cy.intercept(`${BASE_URL}/offer`, (req) => {
       req.reply([
         {
           ...mockPlan,
@@ -46,7 +47,7 @@ describe("checkout", () => {
   });
 
   it("should render non-splittable form", () => {
-    cy.intercept(`${baseUrl}/offer`, (req) => {
+    cy.intercept(`${BASE_URL}/offer`, (req) => {
       req.reply([
         {
           ...mockPlan,
@@ -65,5 +66,54 @@ describe("checkout", () => {
     cy.get('input[name="cpf"]').should("be.visible");
     cy.get('input[name="coupon"]').should("be.visible");
     cy.get('select[name="installments"]').should("not.exist");
+  });
+});
+
+describe("/checkout - error", () => {
+  it("should render plans error", () => {
+    cy.intercept(`${BASE_URL}/offer`, (req) => {
+      req.reply({
+        statusCode: 500,
+      });
+    });
+    cy.visit("/checkout");
+
+    cy.contains(
+      "Ocorreu um erro. Atualize a página ou tente novamente mais tarde.",
+      { timeout: 30 * 1_000 } // increase timeout as react query will retry a few times
+    ).should("be.visible");
+  });
+
+  it("should render subscription error", () => {
+    const checkout = {
+      number: "1234123412341234",
+      expiration: "1212",
+      code: "123",
+      name: "usuario",
+      cpf: "12312312312",
+      coupon: "1234",
+      installments: "12",
+      plan: "Premium Anual | Parcelado",
+    };
+
+    cy.intercept(`${BASE_URL}/subscription`, (req) => {
+      req.reply({
+        statusCode: 500,
+      });
+    });
+    cy.visit("/checkout");
+
+    cy.get('input[name="number"]').type(checkout.number);
+    cy.get('input[name="expiration"]').type(checkout.expiration);
+    cy.get('input[name="code"]').type(checkout.code);
+    cy.get('input[name="name"]').type(checkout.name);
+    cy.get('input[name="cpf"]').type(checkout.cpf);
+    cy.get('input[name="coupon"]').type(checkout.coupon);
+    cy.get('select[name="installments"]').select(checkout.installments);
+    cy.contains(checkout.plan).click();
+    cy.contains("Finalizar pagamento").click();
+    cy.contains(
+      "Ocorreu um erro. Atualize a página ou tente novamente mais tarde."
+    ).should("be.visible");
   });
 });
